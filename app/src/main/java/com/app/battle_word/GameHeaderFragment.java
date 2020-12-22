b656c62064aebfa84ff9f7b706abbcd88c1c7d12
EricM <guyericmodjo@gmail.com>
2020-12-22 10:16:22 +0100

    Adding game scenario page

--- app/src/main/java/com/app/battle_word/GameHeaderFragment.java
+++ app/src/main/java/com/app/battle_word/GameHeaderFragment.java
@@ -7,58 +7,72 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GameHeaderFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GameHeaderFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public GameHeaderFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GameHeaderFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GameHeaderFragment newInstance(String param1, String param2) {
-        GameHeaderFragment fragment = new GameHeaderFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ImageView life1;
+    private ImageView life2;
+    private ImageView life3;
+    private ImageView life4;
+    private ImageView life5;
+    private ImageView life6;
+    private ImageView life7;
+    private ImageView life8;
+    private ImageView life9;
+    private ImageView life10;
+    private ImageView led1;
+    private ImageView led2;
+    private ImageView led3;
+    private ImageView led4;
+    private ImageView led5;
+    private ImageView led6;
+    private ImageView led7;
+    private ImageView led8;
+    private ImageView led9;
+    private ImageView led10;
+    private Button settingsButton;
+    private ProgressBar timeProgressBar;
+    private TextView scoreTextView;
+    private TextView stageTextView;
+    private int  numLifes= 5;
+    private ImageView[] leds;
+    private ImageView[] lifes;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_header, container, false);
+        View v =inflater.inflate(R.layout.fragment_game_header, container, false);
+        life1 = v.findViewById(R.id.life_1);
+        life2 = v.findViewById(R.id.life_2);
+        life3 = v.findViewById(R.id.life_3);
+        life4 = v.findViewById(R.id.life_4);
+        life5 = v.findViewById(R.id.life_5);
+        life6 = v.findViewById(R.id.life_6);
+        life7 = v.findViewById(R.id.life_7);
+        life8 = v.findViewById(R.id.life_8);
+        life9 = v.findViewById(R.id.life_9);
+        life10 = v.findViewById(R.id.life_10);
+        led1 = v.findViewById(R.id.word_found_led_1);
+        led2 = v.findViewById(R.id.word_found_led_2);
+        led3 = v.findViewById(R.id.word_found_led_3);
+        led4 = v.findViewById(R.id.word_found_led_4);
+        led5 = v.findViewById(R.id.word_found_led_5);
+        led6 = v.findViewById(R.id.word_found_led_6);
+        led7 = v.findViewById(R.id.word_found_led_7);
+        led8 = v.findViewById(R.id.word_found_led_8);
+        led9 = v.findViewById(R.id.word_found_led_9);
+        led10 = v.findViewById(R.id.word_found_led_10);
+        leds = new ImageView[]{led1,led2,led3,led4,led5, led6, led7, led8,led9,led10};
+        lifes = new ImageView[]{life1,life2,life3,life4,life5,life6,life7,life8,life9,life10};
+        timeProgressBar = v.findViewById(R.id.time_progressbar);
+        settingsButton = v.findViewById(R.id.settings_button);
+        scoreTextView  = v.findViewById(R.id.score);
+        stageTextView = v.findViewById(R.id.stage_value);
+        return  v;
     }
 }
