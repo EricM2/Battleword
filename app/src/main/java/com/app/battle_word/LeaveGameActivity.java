eb17018890c5d955f5ad8ec5dfc35ca2fe10d29c
EricM <guyericmodjo@gmail.com>
2021-01-03 18:06:32 +0100

    new first

---
+++ app/src/main/java/com/app/battle_word/LeaveGameActivity.java
@@ -0,0 +1,14 @@
+package com.app.battle_word;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeaveGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leave_game);
+    }
+}
